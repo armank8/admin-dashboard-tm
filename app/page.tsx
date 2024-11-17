172c3ab6a4988292e31190e8a1cd5c2c16c69fb9
Arman-Kabir <armankabir8@gmail.com>
2024-11-17 12:15:09 +0600

    Button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
       <h1 className="text-2xl text-center">Dashboard</h1>
+      <Button variant='destructive' size='lg' className="text-blue-400">CLick Me</Button>
     </>
   );
 }
