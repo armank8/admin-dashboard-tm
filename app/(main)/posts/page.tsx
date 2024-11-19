cfef6848fc36a1e9ca4fdb23961440104d60caf7
Arman-Kabir <armankabir8@gmail.com>
2024-11-19 18:42:14 +0600

    posts

---
+++ app/(main)/posts/page.tsx
@@ -0,0 +1,9 @@
+import PostsTable from "@/components/posts/PostsTable";
+
+const PostsPage = () => {
+    return ( <div>
+        <PostsTable></PostsTable>
+    </div> );
+}
+ 
+export default PostsPage;
