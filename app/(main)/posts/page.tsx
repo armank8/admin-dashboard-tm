ac4c8c9f4fdc10f5ae036dab82f710ef0e2b2023
Arman-Kabir <armankabir8@gmail.com>
2024-11-19 19:07:49 +0600

    BackByutton

--- app/(main)/posts/page.tsx
+++ app/(main)/posts/page.tsx
@@ -1,7 +1,9 @@
 import PostsTable from "@/components/posts/PostsTable";
+import BackButton from "@/components/BackButton";
 
 const PostsPage = () => {
     return ( <div>
+        <BackButton text='Go Back' link='/'></BackButton>
         <PostsTable></PostsTable>
     </div> );
 }
