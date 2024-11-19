18fb90489f32c348b1e1c09a7d8c50ef28ef5beb
Arman-Kabir <armankabir8@gmail.com>
2024-11-19 19:26:24 +0600

    postsPagination

--- app/(main)/posts/page.tsx
+++ app/(main)/posts/page.tsx
@@ -1,10 +1,12 @@
 import PostsTable from "@/components/posts/PostsTable";
 import BackButton from "@/components/BackButton";
+import PostsPagination from "@/components/posts/PostsPagination";
 
 const PostsPage = () => {
     return ( <div>
         <BackButton text='Go Back' link='/'></BackButton>
         <PostsTable></PostsTable>
+        <PostsPagination></PostsPagination>
     </div> );
 }
  
