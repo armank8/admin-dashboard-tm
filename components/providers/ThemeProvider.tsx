f7f7b5393ff41e0bdf76c8231457418a4eb9cc33
Arman-Kabir <armankabir8@gmail.com>
2024-11-24 14:19:01 +0600

    [SOlved] Hydration erros{MArker}

--- components/providers/ThemeProvider.tsx
+++ components/providers/ThemeProvider.tsx
@@ -1,14 +1,26 @@
-"use client"
+"use client";
 import dynamic from "next/dynamic";
 
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useState, useEffect } from "react";
 
 export function ThemeProvider({
-// function ThemeProvider({
+  // function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  const [loaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+  if (!loaded) {
+    return (
+      <div className="flex flex-col items-center justify-center gap-4 p-4 text-4xl">
+        Loading...
+      </div>
+    );
+  }
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
 
 // export default dynamic (() => Promise.resolve(ThemeProvider), {ssr: false})
